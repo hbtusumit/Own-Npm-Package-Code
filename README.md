198f0694040ffed5e183a39db9b1344f6496a34f
Sumit <sumitpathak476@gmail.com>
2021-05-26 13:56:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a  module available through the npm registry. Installation is done using the npm install command:
+        $ npm install  own-calculator
+API
+    var cal = require('own-calculator')
+    
+For Result
+     cal.add(4,5);
+     cal.mul(4,2);# Own-Npm-Package-Code
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports ={
+    add(x,y){
+        var z;
+        z=x+y;
+        return z;
+    },
+    mul(x,y){
+        var z;
+        z=x*y;
+        return z;
+    },
+    sub(x,y){
+        var z;
+        z=x-y;
+        return z;
+    },
+    div(x,y){
+        var z;
+        z=x/y;
+        return z;
+    }
+}
